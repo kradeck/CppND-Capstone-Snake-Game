f1aea25da5134f26d4e656b3669de8543509afb9
Krzysztof Radecki <radeckikrzysztof@gmail.com>
2020-04-19 07:14:42 -0700

    create event objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <memory>
 #include "controller.h"
 #include "game.h"
 #include "renderer.h"
 #include "config_file_parser.hpp"
 #include "event_list.hpp"
+#include "event.hpp"
 
 // I don't want the ConfigFileParser object alive for the whole program
 // so with the variables scope rules tha usage is in a separate function
@@ -32,12 +34,84 @@ std::vector<std::pair<std::string, std::string>> load_config(const std::string &
   }
 }
 
+EventList<std::unique_ptr<BaseEvent>> create_events(std::vector<std::pair<std::string, std::string>> && params)
+{
+  const float speed_up{0.04f};
+  const float speed_down{-0.04f};
+  // bonus can also be negative so int is used instead of unsigned,
+  // but in this version only positive bonus usage is provided
+  const int score_bonus{10}; 
+  
+  using ptr_e = std::unique_ptr<BaseEvent>;
+
+  EventList<ptr_e> list;
+  // if the wrong value is provided - exception is thrown,
+  // no reason to parse and load events further
+  try {
+    for(auto & event : params)
+    {
+      // casting unsigned long to unsigned - no matter if uunsigned long
+      // is reduced to unsigned range
+      auto score_trigger{static_cast<unsigned>(std::stoul(event.second))};
+
+      if(event.first == "speed_up")
+      {
+        ptr_e event = std::make_unique<SpeedEvent>(score_trigger, speed_up);
+        list.add(std::move(event));
+      }
+      else if(event.first == "speed_down")
+      {
+        ptr_e event = std::make_unique<SpeedEvent>(score_trigger, speed_down);
+        list.add(std::move(event));
+      }
+      else if(event.first == "extra_score")
+      {
+        ptr_e event = std::make_unique<ScoreEvent>(score_trigger, score_bonus);
+        list.add(std::move(event));
+      }
+      else if(event.first == "reverse_keyboard")
+      {
+        ptr_e event = std::make_unique<KeyboardEvent>(score_trigger);
+        list.add(std::move(event));
+      }
+      else 
+      {
+        throw(std::runtime_error("unknown event name"));
+      }    
+    } // for(auto & event : params)
+    return list;
+
+  } catch (std:: invalid_argument & e) {
+      std::cout << "please check if the event config file is correct: "
+                << e.what() << "\n";
+      // empty list is returned - the game should run without events
+      return EventList<ptr_e>{};
+    } catch (std::out_of_range & e) {
+      std::cout << "please check if the event config file is correct: "
+                << e.what() << "\n";
+      // empty list is returned - the game should run without events
+      return EventList<ptr_e>{};
+    } catch (std::runtime_error & e) {
+      std::cout << "please check if the event config file is correct: "
+                << e.what() << "\n"; 
+      // empty list is returned - the game should run without events 
+      return EventList<ptr_e>{};
+    } catch (...) {
+      std::cout << "please check if the event config file is correct: "
+                << "unknown error\n";
+      // empty list is returned - the game should run without events
+      return EventList<ptr_e>{};
+    }
+}
+
 void get_events(std::string & fileName)
 {
   auto params = load_config(fileName);
-  for(auto & e : params)
+  auto list = std::move(create_events(std::move(params)));  
+
+  for(auto & l : list)
   {
-    std::cout << e.first <<" " << e.second << "\n";
+    (*l)();
   }
 }
 
